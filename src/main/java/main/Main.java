c299b9921fc1bafd6f594459d64ad1a4d442c90b
Roberta <roberta_stefana1998@yahoo.com>
2020-07-26 13:58:23 +0300

    Extend SpringBootServletInitializer

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,11 +2,10 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
